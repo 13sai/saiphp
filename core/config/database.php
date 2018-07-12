04361f4f24ee2e06c3b7a99dcb52f1b35eb4e4a1
Zeta <957042781@qq.com>
2018-07-12 16:50:47 +0800

    Delete database.php

--- core/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'database_type' => 'mysql',
-    'database_name' => 'bdm258386283_db',
-    'server' => 'bdm258386283.my3w.com',
-    'username' => 'bdm258386283',
-    'password' => '13sai171106',
-    'charset' => 'utf8'
-);
